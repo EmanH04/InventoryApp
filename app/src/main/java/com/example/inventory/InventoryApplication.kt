c3a7d386ee93c3ab7b2d42a9d602e432ab2d65c9
EmanH04 <eman.hgs@eduge.ch>
2023-03-08 11:29:01 +0100

    First commit

---
+++ app/src/main/java/com/example/inventory/InventoryApplication.kt
@@ -0,0 +1,16 @@
+/*
+Eman
+ */
+package com.example.inventory
+
+import android.app.Application
+
+import com.example.inventory.data.ItemRoomDatabase
+
+class InventoryApplication : Application(){
+    val database: ItemRoomDatabase by lazy { ItemRoomDatabase.getDatabase(this) }
+
+}
+
+
+
